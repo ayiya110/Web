8841fcf4d76c4fcb9bccb9076e00c7c015f764b5
DESKTOP-65SAUST\Alumno <informatica@esne.es>
2025-01-27 21:10:42 +0100

    a

--- JS/Settings.js
+++ JS/Settings.js
@@ -2,22 +2,6 @@ $(document).ready(function(){
     console.log("Documento cargado");
 
 
-    // Código de los sliders
-    $("#slider").slider({
-        min: 0,          // Valor mínimo
-        max: 100,        // Valor máximo
-        value: 50,       // Valor inicial
-        slide:function(event, ui) {
-            // Actualizar el valor mostrado en la página
-            $("#valorSlider").text(ui.value);
-            
-            // Guardar el valor en una variable
-            var valorDelSlider = ui.value;
-            console.log("Valor del Slider: " + valorDelSlider); // Aquí puedes hacer lo que quieras con el valor
-        }
-    });
-
-
     // Hace que funcione el botón de return
     let botonReturn = $("#return");
     botonReturn.click(function(){

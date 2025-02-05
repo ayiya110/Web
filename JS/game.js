895d4433b1b95bff7a9d89a1f08fedfe0edeb749
DESKTOP-65SAUST\Alumno <Tv04Franky@gmail.com>
2025-02-05 15:48:53 +0100

    Funcionalidad Play
    Tb se ha quitado status.html

--- JS/game.js
+++ JS/game.js
@@ -1,6 +1,22 @@
 $(document).ready(function(){
-    let buttonSettings = $("#settings");
-    buttonSettings.click(function(){
-        console.log("ready");
-    })   
+    console.log("Documento cargado");
+
+      // Hace que funcione el botón de return
+      let PlayLink = $("#play");
+      PlayLink.click(function(){
+          window.location.href="../JuegoHTML/play.html";
+      }); 
+      let SettingsLink = $("#settings");
+      SettingsLink.click(function(){
+          window.location.href="../JuegoHTML/settings.html";
+      }); 
+      let CostumesLink = $("#costumes");
+      CostumesLink.click(function(){
+          window.location.href="../JuegoHTML/costumes.html";
+      }); 
+      let PageLink = $("#quit");
+      PageLink.click(function(){
+          window.location.href="../JuegoHTML/inde.html";
+          st
+      }); 
 })

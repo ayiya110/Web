519d81d35922bdb7021d962ff753b883d5b0cca8
DESKTOP-65SAUST\Alumno <126577362+Cefauro@users.noreply.github.com>
2025-02-05 15:51:07 +0100

    Slider colorido

--- JS/costumes.js
+++ JS/costumes.js
@@ -10,3 +10,27 @@ $(document).ready(function () {
     // Evento de cambio en los sliders
     $("input[type='range']").on("input", updateCharacterStyle);
 });
+
+
+// Para revisar
+const rangeInput = document.querySelector('.js-range-input');
+const output = document.querySelector('.js-range-output');
+const root = document.documentElement;
+
+function setHue() {
+  output.value = rangeInput.value + '°';
+  root.style.setProperty('--hue', rangeInput.value);
+}
+
+function setDefaultState() {
+  rangeInput.focus();
+  setHue();
+}
+
+rangeInput.addEventListener('input', function(){
+  setHue();
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+  setDefaultState();
+});

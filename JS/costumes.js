f50966c0dea17cb2abf0b35ca11dbbdac7afacf0
Cefauro <alonso.ayz@gmail.com>
2025-02-12 21:56:32 +0100

    Colores sliders

--- JS/costumes.js
+++ JS/costumes.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     function updateCharacterStyle() {
-        // let hue = $("#hue").val();
+        let hue = $("#hue").val();
         let saturation = $("#saturation").val();
         let brightness = $("#brightness").val();
 
@@ -34,3 +34,47 @@ $(document).ready(function() {
 
   setDefaultState();
 });
+
+// $(document).ready(function() {
+//     const $rangeInput2 = $(".js-range-input2");
+//     // const $output = $('.js-range-output');
+//     const $root2 = $(":root");
+  
+//     function setSaturation() {
+//         // $output.val($rangeInput.val() + '°');
+//         $root2.css("--saturation", $rangeInput2.val());
+//     }
+  
+//     function setDefaultState2() {
+//         $rangeInput2.focus();
+//         setSaturation();
+//     }
+  
+//     $rangeInput2.on("input", function() {
+//         setSaturation();
+//     });
+  
+//     setDefaultState2();
+//   });
+
+//   $(document).ready(function() {
+//     const $rangeInput3 = $(".js-range-input3");
+//     // const $output = $('.js-range-output');
+//     const $root3 = $(":root");
+  
+//     function setBrightness() {
+//         // $output.val($rangeInput.val() + '°');
+//         $root3.css("--brightness", $rangeInput3.val());
+//     }
+  
+//     function setDefaultState3() {
+//         $rangeInput3.focus();
+//         setBrightness();
+//     }
+  
+//     $rangeInput3.on("input", function() {
+//         setBrightness();
+//     });
+  
+//     setDefaultState3();
+//   });

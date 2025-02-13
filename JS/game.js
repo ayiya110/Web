616bc1bee612fc6e099dcf75f33ba10b6a34caaa
DESKTOP-65SAUST\Alumno <Tv04Franky@gmail.com>
2025-02-13 12:58:18 +0100

    backup_2

--- JS/game.js
+++ JS/game.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
     console.log("Documento cargado");
-    
+
+      // Hace que funcione el botón de return
+      let PlayLink = $("#play");
+      PlayLink.click(function(){
+          window.location.href="../JuegoHTML/play.html";
+      }); 
+      let SettingsLink = $("#settings");
+      SettingsLink.click(function(){
+          window.location.href="../JuegoHTML/settings.html";
+      }); 
+      let CostumesLink = $("#costumes");
+      CostumesLink.click(function(){
+          window.location.href="../JuegoHTML/costumes.html";
+      }); 
+      let PageLink = $("#quit");
+      PageLink.click(function(){
+          window.location.href="../index.html";
+          st
+      }); 
 })
